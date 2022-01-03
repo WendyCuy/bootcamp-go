88a57963fb2d4a806f3a5a44c51cc43106e75b74
WendyCuy <wendy.cuy@mercadolibre.com.co>
2022-01-02 20:13:08 -0500

    Se agrega Json y se actualiza main

--- go-web/clase1_tm/main.go
+++ go-web/clase1_tm/main.go
@@ -1,8 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Productos struct {
+	Id        string
+	Nombre    string
+	Color     string
+	Precio    int
+	Stock     int
+	Codigo    string
+	Publicado bool
+	Fecha     string
+}
 
 func main() {
-	fmt.Println("hola")
+	router := gin.Default()
+
+	// Captura la solicitud GET "Hola nombre"
+	router.GET("hola/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.JSON(200, gin.H{
+			"message": "Hola " + name,
+		})
+	})
+
+	datosJson, err := ioutil.ReadFile("products.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	productos := Productos{}
+	err = json.Unmarshal(datosJson, &productos)
+	if err != nil {
+		log.Fatal(err)
+	}
+	router.GET("/productos", func(ctx *gin.Context) {
+		ctx.JSON(200, productos)
+	})
+
+	router.Run(":8080")
 
 }
